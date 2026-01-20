2dc2edce2d61ea531ebbf6a140012e9bb36e1e2e
Yiwei Ren <holyddlxjtu@gmail.com>
2026-01-20 16:26:23 +0800

    feat: add compute_lu_factors to direct solvers import

--- bempp_cl/api/linalg/__init__.py
+++ bempp_cl/api/linalg/__init__.py
@@ -6,4 +6,4 @@ can be found at https://bempp.com/handbook/api/solvers.html
 
 from .iterative_solvers import gmres
 from .iterative_solvers import cg
-from .direct_solvers import lu
+from .direct_solvers import lu, compute_lu_factors
